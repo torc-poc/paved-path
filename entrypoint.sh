352c710461aed44306bc6010859a8b986b7d102c
Scott Tully <scott.tully@gmail.com>
2025-06-25 08:01:11 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,5 +9,6 @@ echo "🔐 AAP_TOKEN = [REDACTED]"
 echo "📄 Workflow file: $WORKFLOW_FILE"
 
 export AAP_TOKEN=$AAP_TOKEN
+export AAP_HOST=$AAP_HOST
 
 python3 $GITHUB_ACTION_PATH/register_workflow_template.py "$WORKFLOW_FILE"
